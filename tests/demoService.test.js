661cda9c77d0b62175f429b3cf3e03d999f21b24
hamza=sharif-glean <hamza.sharif@glean.com>
2025-05-20 23:32:18 -0500

    chore: initial scaffold of widget-service

---
+++ tests/demoService.test.js
@@ -0,0 +1,21 @@
+const service = require('../services/demoService');
+
+describe('demoService', () => {
+  it('returns all widgets', async () => {
+    const all = await service.getAll();
+    expect(Array.isArray(all)).toBe(true);
+  });
+
+  it('returns a widget by id', async () => {
+    const widget = await service.getById(1);
+    expect(widget).toHaveProperty('id', 1);
+  });
+
+  it('creates a new widget', async () => {
+    const before = (await service.getAll()).length;
+    const newW = await service.create({ name: 'Widget C' });
+    expect(newW).toHaveProperty('id', before + 1);
+    expect((await service.getAll()).length).toBe(before + 1);
+  });
+});
+
